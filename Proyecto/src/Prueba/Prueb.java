bb02372c3f7ef5c540f1add0ae0cf7ea8c3c9194
ayoub <ayoub@LAPTOP-20KJKROS>
2024-05-06 21:11:34 +0200

    Prueba

---
+++ Proyecto/src/Prueba/Prueb.java
@@ -0,0 +1,14 @@
+package Prueba;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class Prueb {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
